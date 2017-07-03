7f050117334779105fd274b0f8121152256f825e
Ricky Hanna <ricky@revunit.com>
2017-07-03 12:32:52 -0500

    Add WSDL

--- app/middleware/logon_pixel.rb
+++ app/middleware/logon_pixel.rb
@@ -9,15 +9,11 @@ class LogonPixel
   end
 
   def _call(env)
-    #require 'pry'; binding.pry
-
     if env.fetch('PATH_INFO') =~ /^\/slp\/(.+)\.png/
       Rails.logger.send(:info, "MIDDLEWARE FOR PNG")
       [ 200, { 'Content-Type' => 'image/png' }, [ File.read(File.join(File.dirname(__FILE__), 'slp.png')) ] ]
     else
       Rails.logger.send(:info, "MIDDLEWARE FOR APP")
-      # @status, @headers, @response = @app.call(env)
-      # [@status, @headers, @response]
       @app.call(env)
     end
   end
